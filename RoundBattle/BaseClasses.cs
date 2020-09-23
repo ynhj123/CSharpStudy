1e53674a2bf1427c0894e9520ec530bd8dc19bbe
yangniuhaojiang <542182536@qq.com>
2020-09-23 11:37:42 +0800

    add roundbattle

---
+++ RoundBattle/BaseClasses.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RoundBattle
+{
+    enum SkillType
+    {
+        Damage,
+        DamageOverTime,
+        Heal,
+        Execute,
+        Invincible,
+    }
+
+    enum StateType
+    {
+        DamageOverTime,
+        Invincible,
+    }
+}
+
+namespace RoundBattle
+{
+    class Character
+    {
+        public static Random random = null;
+
+        public string name;
+        public int hp;
+        public List<Skill> skills = new List<Skill>();
+
+        // 正在生效的状态
+        public List<State> states = new List<State>();
+
+        public bool AddState(State state)
+        {
+            states.Add(state);
+            return true;
+        }
+
+        public bool HasState(StateType state_type)
+        {
+            for (int i = 0; i < states.Count; ++i)
+            {
+                if (states[i].time > 0 && states[i].type == state_type)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void AddSkill(Skill skill)
+        {
+            skills.Add(skill);
+        }
+
+        public Skill GetSkill(int skill_index)
+        {
+            Skill skill = skills[skill_index];
+            return skill;
+        }
+
+        public Skill RandSkill()
+        {
+            int r = random.Next(skills.Count);
+            return GetSkill(r);
+        }
+
+        private void CostHp(int n)
+        {
+            if (IsInvincible())
+            {
+                return;
+            }
+            hp -= n;
+            if (hp < 0)
+            {
+                hp = 0;
+            }
+        }
+
+        public bool BeHit(Skill skill)
+        {
+            if (hp <= 0)
+            {
+                return false;
+            }
+            switch (skill.type)
+            {
+                case SkillType.Damage:
+
+                    CostHp(skill.damage);
+                    break;
+                case SkillType.DamageOverTime:
+                    State temp = new State(StateType.DamageOverTime, skill.time_DOT, skill.damage_DOT);
+                    temp.name = skill.name;
+                    AddState(temp);
+                    break;
+                case SkillType.Heal:
+                    hp += skill.heal;
+                    break;
+                case SkillType.Execute:
+                    CostHp(hp);
+                    break;
+                case SkillType.Invincible:
+                    State invincible = new State(StateType.Invincible, skill.time);
+                    invincible.name = skill.name;
+                    AddState(invincible);
+                    break;
+            }
+            return true;
+        }
+
+        public bool IsInvincible()
+        {
+            foreach (var state in states)
+            {
+                if ("无敌".Equals(state.name) && state.time != 0)
+                {
+
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        public List<State> StateTakeEffect()
+        {
+            List<State> effect_states = new List<State>();
+            foreach (State state in states)
+            {
+                effect_states.Add(state);
+                switch (state.type)
+                {
+                    case StateType.DamageOverTime:
+                        CostHp(state.damage);
+                        state.time -= 1;
+                        break;
+                    case StateType.Invincible:
+                        state.time -= 1;
+                        break;
+                }
+            }
+            for (int i = states.Count - 1; i >= 0; i--)
+            {
+                if (states[i].time == 0)
+                {
+                    states.RemoveAt(i);
+                }
+            }
+            return effect_states;
+        }
+
+        public bool UseSkill(Skill skill, Character target_cha)
+        {
+            switch (skill.type)
+            {
+                case SkillType.Execute:
+                    if (target_cha.hp >= skill.max_hp)
+                    {
+                        return false;
+                    }
+                    return true;
+                default:
+                    return true;
+            }
+        }
+    }
+
+    class Skill
+    {
+        public string name;
+        public SkillType type;
+
+        // 攻击类型
+        public int damage;
+
+        // 治疗类型
+        public int heal;
+
+        // DOT类型
+        public int time_DOT;
+        public int damage_DOT;
+
+        // 处决类型
+        public int max_hp;
+
+        // 无敌类型
+        public int time;
+
+        public static Skill CreateDamageSkill(string name, int damage)
+        {
+            Skill skill = new Skill();
+            skill.name = name;
+            skill.type = SkillType.Damage;
+            skill.damage = damage;
+
+            return skill;
+        }
+
+        public static Skill CreateDOTSkill(string name, int time, int damage)
+        {
+            Skill skill = new Skill();
+            skill.name = name;
+            skill.type = SkillType.DamageOverTime;
+            skill.time_DOT = time;
+            skill.damage_DOT = damage;
+            return skill;
+        }
+
+        public static Skill CreateHealSkill(string name, int heal)
+        {
+            Skill skill = new Skill();
+            skill.name = name;
+            skill.type = SkillType.Heal;
+            skill.heal = heal;
+            return skill;
+        }
+
+        public static Skill CreateExecuteSkill(string name, int hp)
+        {
+            Skill skill = new Skill();
+            skill.name = name;
+            skill.type = SkillType.Execute;
+            skill.max_hp = hp;
+            return skill;
+        }
+        public static Skill CreateInvincibleSkill(string name, int time)
+        {
+            Skill skill = new Skill();
+            skill.name = name;
+            skill.type = SkillType.Invincible;
+            skill.time = time;
+            return skill;
+        }
+    }
+
+    class State
+    {
+        public StateType type;
+        public string name;
+        public int damage;
+        public int time;
+
+        public State()
+        {
+        }
+
+        public State(StateType t, int d1, int d2)
+        {
+            type = t;
+            switch (t)
+            {
+                case StateType.DamageOverTime:
+                    time = d1;
+                    damage = d2;
+                    break;
+
+
+            }
+        }
+        public State(StateType t, int d1)
+        {
+            type = t;
+            switch (t)
+            {
+                case StateType.Invincible:
+                    time = d1;
+                    break;
+            }
+        }
+
+        public override string ToString()
+        {
+            return name + "(" + time + ")";
+        }
+
+        public State Copy()
+        {
+            State s = new State();
+            s.type = type;
+            s.name = name;
+            s.damage = damage;
+            s.time = time;
+            return s;
+        }
+    }
+
+}
---
+++ RoundBattle/RoundBattle.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 回合制对战游戏。
+// 用面向对象方式实现，数据与逻辑分离，具有一定的扩展性
+
+
+// 练习：
+// 1、理解战斗的过程。试着改变主角和NPC，改变他们的技能。（只需要修改关卡初始化就可以实现）
+
+// 2、“框架”就是用来解决扩展和规范性问题的，试着添加新的怪物和主角。可以在每关随机出不同的怪物
+
+// （以上2个改动不需要调整底层代码。）
+
+// 3、本程序中，技能可以增加新的类型，比如无敌N回合的技能。试着实现这个技能。
+
+// 4、扩展性是相对的，有时超出最初设计的话就要改动底层了。
+//    回血技能没有次数限制，不合理，试着加上次数限制。
+
+// 5、BUG：已经时间为0的状态，不会被删除
+
+// 6、思考一下：持续掉血、持续加血、持续无敌，这类加状态的技能，能否用一种技能类型代替？
+// 可能需要给状态类，加一个Copy()方法
+
+
+namespace RoundBattle
+{
+    class RoundBattle
+    {
+        static Random random = new Random();
+
+        static Character player;
+        static Character monster;
+
+        // ------关卡设置------
+        static void InitStage()
+        {
+
+            int randoInt = random.Next(0, 2);
+            if (randoInt == 0)
+            {
+                monster = CreateSkeleton();
+
+            }
+            else
+            {
+                monster = CreateSkeletonKing();
+            }
+        }
+
+        static Character CreateSimon()
+        {
+            Character simon = new Character();
+            simon.name = "西蒙";
+            simon.hp = 100;
+            simon.AddSkill(Skill.CreateDamageSkill("出拳", 20));
+            simon.AddSkill(Skill.CreateExecuteSkill("处决", 30));
+            simon.AddSkill(Skill.CreateDOTSkill("圣水", 8, 4));
+            simon.AddSkill(Skill.CreateHealSkill("吃鸡腿", 50));
+            simon.AddSkill(Skill.CreateInvincibleSkill("无敌", 3));
+            return simon;
+        }
+
+        static Character CreateSkeleton()
+        {
+            Character monster = new Character();
+            monster.name = "骷髅兵";
+            monster.hp = 50;
+            monster.AddSkill(Skill.CreateDamageSkill("扔骨头", 20));
+            monster.AddSkill(Skill.CreateHealSkill("死亡之舞", 30));
+            return monster;
+        }
+        static Character CreateSkeletonKing()
+        {
+            Character monster = new Character();
+            monster.name = "骷髅王";
+            monster.hp = 100;
+            monster.AddSkill(Skill.CreateDamageSkill("扔骨头", 50));
+            monster.AddSkill(Skill.CreateHealSkill("死亡之舞", 80));
+            return monster;
+        }
+        // -------------------
+
+        // -------回合内函数-----
+        static void ShowSkills(Character cha)
+        {
+            for (int i = 0; i < cha.skills.Count; ++i)
+            {
+                Console.WriteLine("{0}. {1}", i + 1, cha.skills[i].name);
+            }
+        }
+
+        static Skill InputSkill(List<Skill> skills)
+        {
+            int n = -1;
+            // 用户输入的是从1开始
+            while (n <= 0 || n > skills.Count)
+            {
+                ConsoleKeyInfo keyinfo = Console.ReadKey();
+                int.TryParse(keyinfo.KeyChar.ToString(), out n);
+            }
+
+            return skills[n - 1];
+        }
+
+        static void ShowInfo(Character cha)
+        {
+            Console.WriteLine("{0}的信息：", cha.name);
+            Console.Write("    HP:{0}  ", cha.hp);
+            for (int i = 0; i < cha.states.Count; ++i)
+            {
+                Console.Write("状态:{0} ", cha.states[i]);
+            }
+            Console.WriteLine();
+        }
+
+        static bool AllChasAlive(List<Character> l)
+        {
+            foreach (Character cha in l)
+            {
+                if (cha.hp <= 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void ChaAct(Character cur_cha, Character other_cha)
+        {
+            if (cur_cha == player)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            var effect_states = cur_cha.StateTakeEffect();
+            for (int i = 0; i < effect_states.Count; ++i)
+            {
+                Console.WriteLine("{0}的{1}状态生效，当前HP:{2}", cur_cha.name, effect_states[i].name, cur_cha.hp);
+            }
+
+            Skill skill = null;
+            // 选择技能的方式不同
+            if (cur_cha == player)
+            {
+                while (true)
+                {
+                    Console.WriteLine("====请选择{0}的技能：", cur_cha.name);
+                    ShowSkills(cur_cha);
+                    skill = InputSkill(player.skills);
+                    if (!cur_cha.UseSkill(skill, other_cha))
+                    {
+                        Console.WriteLine("技能使用条件不满足, 重新选择");
+                        continue;
+                    }
+                    break;
+                }
+            }
+            else
+            {
+                skill = monster.RandSkill();        // 没有UseSkill，BUG
+                Console.WriteLine("{0}使用了'{1}'技能", monster.name, skill.name);
+            }
+
+            // 判断攻击谁，不应该根据技能类型判断，而是用一个字段来判断
+            if (skill.type == SkillType.Heal)
+            {
+                if (cur_cha.BeHit(skill))
+                {
+                    if (skill.type == SkillType.Heal)
+                    {
+                        Console.WriteLine("{0}成功受到治疗，当前HP:{1}", cur_cha.name, cur_cha.hp);
+                    }
+                }
+            }
+            else if (skill.type == SkillType.Invincible)
+            {
+                if (cur_cha.BeHit(skill))
+                {
+                    if (skill.type == SkillType.Invincible)
+                    {
+                        Console.WriteLine("{0}成功无敌了，当前HP:{1}，无敌回合{2}", cur_cha.name, cur_cha.hp, skill.time);
+                    }
+                }
+            }
+
+            else
+            {
+                if (other_cha.BeHit(skill))
+                {
+                    if (skill.type == SkillType.Damage)
+                    {
+                        Console.WriteLine("{0}受到伤害{1}点，当前HP:{2}", other_cha.name, skill.damage, other_cha.hp);
+
+                    }
+                    else if (skill.type == SkillType.DamageOverTime)
+                    {
+                        Console.WriteLine("{0}中了状态", other_cha.name);
+                    }
+                    else if (skill.type == SkillType.Execute)
+                    {
+                        Console.WriteLine("{0}被处决了！", other_cha.name);
+                    }
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        static Character StageBattle()
+        {
+            int round = 1;
+            Character victory_cha;
+
+            while (player.hp > 0 && monster.hp > 0)
+            {
+                Console.WriteLine("--------第{0}回合开始--------------", round);
+
+                ChaAct(player, monster);
+                if (monster.hp <= 0)
+                {
+                    break;
+                }
+                ChaAct(monster, player);
+
+                ShowInfo(player);
+                ShowInfo(monster);
+                Console.WriteLine();
+                round += 1;
+            }
+
+            if (player.hp > 0)
+            {
+                victory_cha = player;
+            }
+            else
+            {
+                victory_cha = monster;
+            }
+            return victory_cha;
+        }
+        // -------------------
+
+        static void Main(string[] args)
+        {
+            Character.random = random;
+            player = CreateSimon();
+
+            int victory_times = 0;
+            while (true)
+            {
+                InitStage();
+                Character victory_cha = StageBattle();
+                if (victory_cha == player)
+                {
+                    Console.WriteLine("{0}战胜了敌人{1}，进入下一关", player.name, monster.name);
+                    victory_times += 1;
+                    Console.WriteLine();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("{0}失败了", player.name);
+            Console.WriteLine("这次冒险一共前进了{0}关", victory_times);
+            Console.WriteLine("GAME OVER");
+
+            Console.ReadLine();
+        }
+    }
+}
